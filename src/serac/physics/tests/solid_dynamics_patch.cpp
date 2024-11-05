1234e7865f9300454f5879c34e415c31ab0523e5
Brandon Talamini <talamini1@llnl.gov>
2024-11-04 20:45:42 -0800

    Update dynamic patch tests

--- src/serac/physics/tests/solid_dynamics_patch.cpp
+++ src/serac/physics/tests/solid_dynamics_patch.cpp
@@ -142,10 +142,8 @@ public:
     auto traction = [material, Hdot](auto, auto n0, auto t) {
       auto                     H = Hdot * t;
       typename Material::State state;  // needs to be reconfigured for mats with state
-      tensor<double, dim, dim> sigma = material(state, H);
-      auto                     F     = Identity<dim>() + H;
-      auto                     J     = det(F);
-      auto                     P     = J * dot(sigma, inv(transpose(F)));
+      tensor<double, dim, dim> P = material(state, H);
+
       // We don't have a good way to restrict the tractions to the
       // complement of the essential boundary segments.
       // The following matches the case when the top and left surfaces
