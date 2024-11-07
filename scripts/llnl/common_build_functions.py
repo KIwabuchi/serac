e9c3e8f6a3e0f0fc4218a997dcaaeac9a8b1ca51
Alex Tyler Chapman <chapman39@llnl.gov>
2024-11-07 14:00:03 -0800

    rename xml file

--- scripts/llnl/common_build_functions.py
+++ scripts/llnl/common_build_functions.py
@@ -328,7 +328,7 @@ def build_and_test_host_config(test_root, host_config, report_to_stdout=False, e
             return res
     else:
         # Copy dummy .xml file to avoid Azure CI warning when not running tests
-        dummy_ctest_src = pjoin(get_repo_dir(), "scripts/azure-pipelines/Test.xml")
+        dummy_ctest_src = pjoin(get_repo_dir(), "scripts/azure-pipelines/DummyTest.xml")
         dummy_ctest_dst = pjoin(build_dir, "Test.xml")
         shutil.copy(dummy_ctest_src, dummy_ctest_dst)
         print("[skipping unit tests]")
