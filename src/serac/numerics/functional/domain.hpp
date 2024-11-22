17f93beef463a2775dfd79a883a339b2e4931500
Brandon Talamini <talamini1@llnl.gov>
2024-11-22 11:22:41 -0800

    Write docstrings

--- src/serac/numerics/functional/domain.hpp
+++ src/serac/numerics/functional/domain.hpp
@@ -37,14 +37,37 @@ struct Domain {
   /// @brief whether the elements in this domain are on the boundary or not
   Type type_;
 
-  /// note: only lists with appropriate dimension (see dim_) will be populated
-  ///       for example, a 2D Domain may have `tri_ids_` and `quad_ids_` non-nonempty,
-  ///       but all other lists will be empty
-  ///
-  /// these lists hold indices into the "E-vector" of the appropriate geometry
-  ///
-  /// @cond
   std::vector<int> vertex_ids_;
+
+  ///@{
+  /// @name ElementIds
+  /// Indices of elements contained in the domain.
+  /// The first set, (vertex_ids_, edge_ids_, ...) hold the index of an element in
+  /// this Domain in the set of all elements of like geometry in the mesh.
+  /// For example, if edge_ids_[0] = 5, then element 0 in this domain is element
+  /// 5 in the grouping of all edges in the mesh. In other words, these lists
+  /// hold indices into the "E-vector" of the appropriate geometry. These are
+  /// used primarily for identifying elements in the domain for participation
+  /// in integrals.
+  ///
+  /// The second set, (mfem_edge_ids_, mfem_tri_ids_, ...), gives the ids of
+  /// elements in this domain in the global mfem::Mesh data structure. These
+  /// maps are needed to find the dofs that live on a Domain.
+  /// 
+  /// Instances of Domain are meant to be homogeneous: only lists with 
+  /// appropriate dimension (see dim_) will be populated by the factory
+  /// functions. For example, a 2D Domain may have `tri_ids_` and `quad_ids_`
+  /// non-empty, but all other lists will be empty.
+  ///
+  /// @note For every entry in the first group (say, edge_ids_), there should
+  /// be a corresponding entry into the second group (mfem_edge_ids_). This
+  /// is an intended invariant of the class, but it's not enforced by the data
+  /// structures. Prefer to use the factory methods (eg, \ref ofElements(...))
+  /// to populate these lists automatically, as they repsect this invariant and
+  /// are tested. Otherwise, use the \ref addElements(...) or addElements(...)
+  /// methods to add new entities, as this requires you to add both entries and
+  /// keep the corresponding lists in sync. You are discouraged from
+  /// manipulating these lists directly.
   std::vector<int> edge_ids_;
   std::vector<int> tri_ids_;
   std::vector<int> quad_ids_;
@@ -56,7 +79,7 @@ struct Domain {
   std::vector<int> mfem_quad_ids_;
   std::vector<int> mfem_tet_ids_;
   std::vector<int> mfem_hex_ids_;
-  /// @endcond
+  ///@}
 
   Domain(const mfem::Mesh& m, int d, Type type = Domain::Type::Elements) : mesh_(m), dim_(d), type_(type) {}
 
@@ -130,14 +153,22 @@ struct Domain {
     exit(1);
   }
 
+  /// @brief get mfem degree of freedom list for a given FiniteElementSpace
+  mfem::Array<int> dof_list(mfem::FiniteElementSpace* fes) const;
+
   /// @brief Add an element to the domain
+  ///
+  /// This is meant for internal use on the class. Prefer to use the factory
+  /// methods (ofElements, ofBoundaryElements, etc) to create domains and
+  /// thereby populate the element lists.
   void addElement(int geom_id, int elem_id, mfem::Geometry::Type element_geometry);
 
   /// @brief Add a batch of elements to the domain
+  ///
+  /// This is meant for internal use on the class. Prefer to use the factory
+  /// methods (ofElements, ofBoundaryElements, etc) to create domains and
+  /// thereby populate the element lists.
   void addElements(const std::vector<int>& geom_id, const std::vector<int>& elem_id, mfem::Geometry::Type element_geometry);
-
-  /// @brief get mfem degree of freedom list for a given FiniteElementSpace
-  mfem::Array<int> dof_list(mfem::FiniteElementSpace* fes) const;
 };
 
 /// @brief constructs a domain from all the elements in a mesh
