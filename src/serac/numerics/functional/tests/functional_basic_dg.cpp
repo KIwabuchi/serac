9db8634d825387c190d423f95f8a408bb90b0f3c
Agent Style <no-reply@llnl.gov>
2024-12-04 16:07:45 -0800

    Apply style updates

--- src/serac/numerics/functional/tests/functional_basic_dg.cpp
+++ src/serac/numerics/functional/tests/functional_basic_dg.cpp
@@ -75,7 +75,10 @@ void L2_test(std::string meshfile)
 }
 
 TEST(basic, L2_test_tris_and_quads_linear) { L2_test<2, 1>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh"); }
-TEST(basic, L2_test_tris_and_quads_quadratic) { L2_test<2, 2>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh"); }
+TEST(basic, L2_test_tris_and_quads_quadratic)
+{
+  L2_test<2, 2>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh");
+}
 
 TEST(basic, L2_test_tets_linear) { L2_test<3, 1>(SERAC_REPO_DIR "/data/meshes/patch3D_tets.mesh"); }
 TEST(basic, L2_test_tets_quadratic) { L2_test<3, 2>(SERAC_REPO_DIR "/data/meshes/patch3D_tets.mesh"); }
@@ -90,11 +93,11 @@ void L2_qoi_test(std::string meshfile)
 
   auto mesh = mesh::refineAndDistribute(buildMeshFromFile(meshfile), 1);
 
-  auto fec = mfem::L2_FECollection(p, dim, mfem::BasisType::GaussLobatto);
+  auto                        fec = mfem::L2_FECollection(p, dim, mfem::BasisType::GaussLobatto);
   mfem::ParFiniteElementSpace fespace(mesh.get(), &fec, dim, serac::ordering);
 
-  int seed = 0;
-  mfem::HypreParVector U = *fespace.NewTrueDofVector();
+  int                  seed = 0;
+  mfem::HypreParVector U    = *fespace.NewTrueDofVector();
   U.Randomize(seed);
 
   // Construct the new functional object using the specified test and trial spaces
@@ -105,31 +108,37 @@ void L2_qoi_test(std::string meshfile)
   Domain interior_faces = InteriorFaces(*mesh);
 
   qoi.AddInteriorFaceIntegral(
-      Dimension<dim-1>{}, DependsOn<0>{},
+      Dimension<dim - 1>{}, DependsOn<0>{},
       [=](double /*t*/, auto X, auto velocity) {
         // compute the unit surface normal
         auto dX_dxi = get<DERIVATIVE>(X);
-        auto n = normalize(cross(dX_dxi));
+        auto n      = normalize(cross(dX_dxi));
 
         // extract the velocity values from each side of the interface
-        // note: the orientation convention is such that the normal 
+        // note: the orientation convention is such that the normal
         //       computed as above will point from from side 1->2
-        auto [u_1, u_2] = velocity; 
+        auto [u_1, u_2] = velocity;
 
         auto a = dot(u_2 - u_1, n);
 
         auto f_1 = u_1 * a;
         auto f_2 = u_2 * a;
         return dot(f_1, f_2);
-      }, interior_faces);
+      },
+      interior_faces);
 
   double t = 0.0;
   check_gradient(qoi, t, U);
-
 }
 
-TEST(basic, L2_qoi_test_tri_and_quads_linear) { L2_qoi_test<2, 1>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh"); }
-TEST(basic, L2_qoi_test_tri_and_quads_quadratic) { L2_qoi_test<2, 2>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh"); }
+TEST(basic, L2_qoi_test_tri_and_quads_linear)
+{
+  L2_qoi_test<2, 1>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh");
+}
+TEST(basic, L2_qoi_test_tri_and_quads_quadratic)
+{
+  L2_qoi_test<2, 2>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh");
+}
 
 TEST(basic, L2_qoi_test_tets_linear) { L2_qoi_test<3, 1>(SERAC_REPO_DIR "/data/meshes/patch3D_tets.mesh"); }
 TEST(basic, L2_qoi_test_tets_quadratic) { L2_qoi_test<3, 2>(SERAC_REPO_DIR "/data/meshes/patch3D_tets.mesh"); }
@@ -140,16 +149,16 @@ TEST(basic, L2_qoi_test_hexes_quadratic) { L2_qoi_test<3, 2>(SERAC_REPO_DIR "/da
 template <int dim, int p>
 void L2_scalar_valued_test(std::string meshfile)
 {
-  using test_space  = L2<p>;
+  using test_space    = L2<p>;
   using trial_space_0 = L2<p>;
   using trial_space_1 = H1<p, dim>;
 
   auto mesh = mesh::refineAndDistribute(buildMeshFromFile(meshfile), 1);
 
-  auto L2fec = mfem::L2_FECollection(p, dim, mfem::BasisType::GaussLobatto);
+  auto                        L2fec = mfem::L2_FECollection(p, dim, mfem::BasisType::GaussLobatto);
   mfem::ParFiniteElementSpace fespace_0(mesh.get(), &L2fec, 1, serac::ordering);
 
-  auto H1fec = mfem::H1_FECollection(p, dim);
+  auto                        H1fec = mfem::H1_FECollection(p, dim);
   mfem::ParFiniteElementSpace fespace_1(mesh.get(), &H1fec, dim, serac::ordering);
 
   mfem::Vector U0(fespace_0.TrueVSize());
@@ -161,36 +170,37 @@ void L2_scalar_valued_test(std::string meshfile)
   // Construct the new functional object using the specified test and trial spaces
   Functional<test_space(trial_space_0, trial_space_1)> residual(&fespace_0, {&fespace_0, &fespace_1});
 
-  constexpr int VALUE = 0;
+  constexpr int VALUE      = 0;
   constexpr int DERIVATIVE = 1;
 
   Domain interior_faces = InteriorFaces(*mesh);
 
   residual.AddInteriorFaceIntegral(
-      Dimension<dim-1>{}, DependsOn<0, 1>{},
+      Dimension<dim - 1>{}, DependsOn<0, 1>{},
       [=](double /*t*/, auto X, auto rho, auto u) {
-
-        auto n = normalize(cross(get<DERIVATIVE>(X))); 
+        auto n = normalize(cross(get<DERIVATIVE>(X)));
 
         auto [rho0, rho1] = rho;
-        auto uTn = dot(get<VALUE>(u), n);
-        auto s = uTn > 0;
+        auto uTn          = dot(get<VALUE>(u), n);
+        auto s            = uTn > 0;
 
-        return serac::tuple{
-          uTn * ((      s) * rho0 + (1.0 - s) * rho1), 
-          uTn * ((1.0 - s) * rho0 + (      s) * rho1)
-        };
-
-      }, interior_faces);
+        return serac::tuple{uTn * ((s)*rho0 + (1.0 - s) * rho1), uTn * ((1.0 - s) * rho0 + (s)*rho1)};
+      },
+      interior_faces);
 
   double t = 0.0;
   check_gradient(residual, t, U0, U1);
-
 }
 
-TEST(basic, L2_mixed_scalar_test_tris_and_quads_linear) { L2_scalar_valued_test<2, 1>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh"); }
+TEST(basic, L2_mixed_scalar_test_tris_and_quads_linear)
+{
+  L2_scalar_valued_test<2, 1>(SERAC_REPO_DIR "/data/meshes/patch2D_tris_and_quads.mesh");
+}
 
-TEST(basic, L2_mixed_scalar_test_tets_and_hexes_linear) { L2_scalar_valued_test<3, 1>(SERAC_REPO_DIR "/data/meshes/patch3D_tets_and_hexes.mesh"); }
+TEST(basic, L2_mixed_scalar_test_tets_and_hexes_linear)
+{
+  L2_scalar_valued_test<3, 1>(SERAC_REPO_DIR "/data/meshes/patch3D_tets_and_hexes.mesh");
+}
 
 int main(int argc, char* argv[])
 {
