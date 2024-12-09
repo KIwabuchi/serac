7b35ae624c1b9ac41b27a86a6257b60ae3eb8341
Brandon Talamini <talamini1@llnl.gov>
2024-12-08 17:45:01 -0800

    Fix docstring

--- src/serac/physics/boundary_conditions/boundary_condition_manager.hpp
+++ src/serac/physics/boundary_conditions/boundary_condition_manager.hpp
@@ -49,6 +49,7 @@ public:
    * @param[in] true_dofs The true degrees of freedom to set with a Dirichlet condition
    * @param[in] ess_bdr_coef The scalar coefficient that evaluates to the Dirichlet condition
    * @param[in] space The finite element space to which the BC should be applied
+   * @param[in] component The component to set (null implies all components are set)
    */
   void addEssential(const mfem::Array<int>& true_dofs, std::shared_ptr<mfem::Coefficient> ess_bdr_coef,
                     mfem::ParFiniteElementSpace& space, std::optional<int> component = {});
