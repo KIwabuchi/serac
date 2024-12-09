8fc6809fc6845141d71758ec5ca2ce27e56e83ea
Brandon Talamini <talamini1@llnl.gov>
2024-12-08 20:45:40 -0800

    Fix compilation error in benchmark problem

--- src/serac/physics/benchmarks/physics_benchmark_solid_nonlinear_solve.cpp
+++ src/serac/physics/benchmarks/physics_benchmark_solid_nonlinear_solve.cpp
@@ -329,9 +329,9 @@ void functional_solid_test_nonlinear_buckle(NonlinSolve nonlinSolve, Prec prec, 
   seracSolid->setMaterial(material, whole_domain);
 
   // fix displacement on side surface
-  serac::Domain side_surface = serac::Domain::ofBoundaryElements(*meshPtr, by_attr<dim>({2, 3, 4, 5}));
+  serac::Domain side_surface = serac::Domain::ofBoundaryElements(*meshPtr, by_attr<DIM>({2, 3, 4, 5}));
   seracSolid->setFixedBCs(side_surface);
-  // seracSolid->setFixedBCs(serac::Domain::ofBoundaryElements(*meshPtr, by_attr<dim>(3));
+  // seracSolid->setFixedBCs(serac::Domain::ofBoundaryElements(*meshPtr, by_attr<DIM>(3));
 
   serac::Domain topSurface = serac::Domain::ofBoundaryElements(*meshPtr, serac::by_attr<DIM>(6));
   // seracSolid->setTraction([&](auto, auto n, auto) { return -loadMagnitude * n; }, topSurface);
