b5ce89b0dcc7b7adec4ddb66b87943df52a5b70c
Alex Tyler Chapman <chapman39@llnl.gov>
2025-01-17 13:49:12 -0800

    add print log run fx

--- scripts/shared-macmini/build-and-test.sh
+++ scripts/shared-macmini/build-and-test.sh
@@ -12,19 +12,27 @@ OUTPUT_LOG="$CI_ROOT_DIR/logs/macmini-build-and-test-$(date +"%Y_%m_%d_%H_%M_%S"
 HOST_CONFIG="$CI_ROOT_DIR/host-configs/firion-darwin-sonoma-aarch64-clang@14.0.6.cmake"
 RECIPIENTS="chapman39@llnl.gov,white238@llnl.gov,talamini1@llnl.gov"
 
+print_run_log(){
+    echo "####################" >> "$OUTPUT_LOG"
+    echo "# $@" >> "$OUTPUT_LOG"
+    echo "####################" >> "$OUTPUT_LOG"
+    "$@" >> "$OUTPUT_LOG" 2>&1
+    echo >> "$OUTPUT_LOG"
+}
+
 # Go to project directory
-cd $PROJECT_DIR
+print_run_log cd $PROJECT_DIR
 
 # Update Serac
-git checkout task/chapman39/macmini-build >> $OUTPUT_LOG 2>&1 # TODO CHANGE TO DEVELOP
-git pull >> $OUTPUT_LOG 2>&1
-git submodule update --init --recursive >> $OUTPUT_LOG 2>&1
+print_run_log git checkout develop
+print_run_log git pull
+print_run_log git submodule update --init --recursive
 
 # Clear previous build(s)
-rm -rf _serac_build_and_test* >> $OUTPUT_LOG 2>&1
+print_run_log rm -rf _serac_build_and_test*
 
 # Build and test Serac
-python3 ./scripts/llnl/build_src.py --host-config $HOST_CONFIG -v -j16 >> $OUTPUT_LOG 2>&1
+print_run_log python3 ./scripts/llnl/build_src.py --host-config $HOST_CONFIG -v -j16
 
 # Email variables
 if [ $? -eq 0 ]; then
@@ -36,4 +44,4 @@ EMAIL_SUBJECT="$EMAIL_SUBJECT MacMini build and test report $(date)"
 EMAIL_BODY="This is automatic weekly report of Serac's MacMini build. See attached for log."
 
 # Send report via email
-echo "$EMAIL_BODY" | mutt -a "$OUTPUT_LOG" -s "$EMAIL_SUBJECT" -- "$RECIPIENTS"
+echo "$EMAIL_BODY" | print_run_log mutt -a "$OUTPUT_LOG" -s "$EMAIL_SUBJECT" -- "$RECIPIENTS"
